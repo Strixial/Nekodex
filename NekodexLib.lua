2c6a328601a35958c8c39569bad30433575aeaa0
Strixial <73123605+Strixial@users.noreply.github.com>
2022-08-09 13:57:13 +0000

    "added some things", (+4738, -3754)

---
+++ NekodexLib.lua
@@ -0,0 +1,49 @@
+local NekodexLib = {}
+
+local RunService = game:GetService("RunService")
+
+NekodexLib.RunLoops = {RenderStepped = {}, Stepped = {}, Heartbeat = {}}
+local RunLoops = NekodexLib.RunLoops
+
+do
+	function RunLoops:BindToRenderStep(Name, Func)
+		if RunLoops.RenderStepped[Name] == nil then
+			RunLoops.RenderStepped[Name] = RunService.RenderStepped:Connect(Func);
+		end
+	end
+
+	function RunLoops:UnbindFromRenderStep(Name)
+		if RunLoops.RenderStepped[Name] ~= nil then
+			RunLoops.RenderStepped[Name]:Disconnect(); 
+			RunLoops.RenderStepped[Name] = nil 
+		end
+	end
+
+	function RunLoops:BindToStepped(Name, Func)
+		if RunLoops.Stepped[Name] == nil then
+			RunLoops.Stepped[Name] = RunService.Stepped:Connect(Func);
+		end
+	end
+
+	function RunLoops:UnbindFromStepped(Name)
+		if RunLoops.Stepped[Name] ~= nil then
+			RunLoops.Stepped[Name]:Disconnect(); 
+			RunLoops.Stepped[Name] = nil 
+		end
+	end
+
+	function RunLoops:BindToHeartbeat(Name, Func)
+		if RunLoops.Heartbeat[Name] == nil then
+			RunLoops.Heartbeat[Name] = RunService.Heartbeat:Connect(Func);
+		end
+	end
+
+	function RunLoops:UnbindFromHeartbeat(Name)
+		if RunLoops.Heartbeat[Name] ~= nil then
+			RunLoops.Heartbeat[Name]:Disconnect(); 
+			RunLoops.Heartbeat[Name] = nil 
+		end
+	end
+end
+
+return NekodexLib
