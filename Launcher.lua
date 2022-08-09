b164feccea488f4ddbf32640dc8048741327adf4
Strixial <73123605+Strixial@users.noreply.github.com>
2022-08-09 14:33:41 +0000

    finished base launcher, fixed things with game json

--- Launcher.lua
+++ Launcher.lua
@@ -27,10 +27,19 @@
     SOFTWARE.
 ]]
 
+-- Prelaunch things
 getgenv().LauncherVersion = "1.0"
-
 local NekodexLib = loadstring(game:HttpGet("https://github.com/Strixial/Nekodex/raw/main/NekodexLib.lua"))()
+local BaseNekodexUrl = "https://raw.githubusercontent.com/Strixial/Nekodex/main/" -- if youre a skid change this to your base url
 
+-- Get game list and current game
 local NekodexGames = game:HttpGet("https://github.com/Strixial/Nekodex/raw/main/NekodexGames.json")
+NekodexGames = NekodexLib.TransformJSON(NekodexGames)
 
-NekodexGames = NekodexLib
+if not NekodexGames[game.PlaceId] then
+    print("Nekodex - Unsupported game!")
+else
+    print("Loading for ", NekodexGames[game.PlaceId]["GameName"])
+    -- get script url and load
+    loadstring(game:HttpGet(BaseNekodexUrl..NekodexGames[game.PlaceId]["CodeDirectory"]))()
+end
